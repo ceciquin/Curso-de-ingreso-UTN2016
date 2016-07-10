2cc0990133e24ec9c6744f7c01b4cdf6f92cfaae
Cecilia Quintana <ceciquin@protonmail.ch>
2016-07-10 17:41:22 -0300

    curso de ingreso UTN, ejercicios de JavaScript.

---
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -0,0 +1,7 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+	alert(" esto funciona de maravillas");
+} //caja de dialogo, la sintaxis es correcta y funciona.
+//se suele utilizar para concatenar datos y mostrar en una ventana emergente.
+
---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var  nombre;
+	nombre= prompt("ingrese su nombre");
+	alert("su nombre es:" + nombre);
+}
+// caja de dialogo prompt ( toma datos)
+// funciona.
