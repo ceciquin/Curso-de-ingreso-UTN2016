25a82176934d75f6e1c047c70031abee93117eb6
ceciquin <ceciquin@protonmail.ch>
2016-07-13 22:05:42 -0300

    switch

---
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -0,0 +1,39 @@
+function Mostrar()
+{
+
+var mes= document.getElementById('mes').value;
+
+switch(mes)
+
+{
+	 case "Enero":
+		case "Febrero":
+			case "Marzo":
+				case "Abril":
+					case "Mayo":
+						case "Junio":
+						
+
+							alert(" falta para el invierno");
+	break;
+			
+	 
+	
+			case "julio":
+				case "Agosto":
+					alert("abrigate que hace frio");
+	break;
+
+			
+	 case "Septiembre":
+			case "Octubre":
+				case "Noviembre":
+					case "Diciembre":
+						alert(" ya pasamos el frio ahora calor");
+	break;
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -0,0 +1,35 @@
+function Mostrar()
+{
+
+var mes= document.getElementById('mes').value;
+
+switch (mes)
+
+{ 
+
+	case "Febrero":
+ 		alert("este mes no tiene mas de 29 dias");
+
+ 		break;
+ 		
+ 			case "Enero":
+ 				case "Marzo":
+ 					case"Abril":
+ 						case "Mayo":
+ 							case "Junio":
+ 								case "Julio":
+ 									case "Agosto":
+ 										case "Septiembre":
+ 											case "Octubre":
+ 												case "Noviembre":
+ 													case "Diciembre":
+ 				alert( " Este mes tiene 30 o mas dias");
+ 						break;
+ 			}
+
+
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
