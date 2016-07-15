dbccd68b4cb3c57ac936362d6eed6d292bfcd729
Cecilia Quintana <ceciquin@protonmail.ch>
2016-07-15 07:18:47 -0300

    entrada y salida

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,9 @@
 	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
 */
 function Mostar()
-{
+{ var nombre;
+	nombre= document.getElementById("elNombre").value;
+		prompt("su nombre es: "+ nombre);
 	
-}
+} // la ventana emergente de promt, se habre para captar el dato.
 
