3dea87f8eef079d637e0db8559481b44113eda59
ceciquin <ceciquin@protonmail.ch>
2016-07-27 22:06:38 -0300

    ejercicio while e isNaN

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,27 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var cantidad;
+
+while (contador < 5)
+
+{ contador = contador +1;
+
+cantidad=prompt("ingrese un numero" + contador);
 
 
+while ( isNaN( cantidad ) )
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	{ cantidad=prompt("ingrese un numero"); }
+
+cantidad= parseInt(cantidad);
+
+acumulador = acumulador + cantidad; 
+
+
+}
+
+ document.getElementById('suma').value=acumulador;
+ document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
