cc184d093f2235d347186adbdac1545f3dacc459
Cecilia Quintana <ceciquin@protonmail.ch>
2016-07-28 20:50:46 -0300

    Ejercicios "switch" hechos

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,7 +1,35 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
+
+switch( mes)
+
+{ 	case "Enero":
+	case"Marzo":
+	case"Mayo":
+	case "Julio":
+	case "Agosto":
+	case"Octubre":
+	case"Diciembre":
+	
+			alert( "tiene 31 dias");
+
+			break;
+
+	case"Febrero":
+
+			alert("tiene 28 dias");
+
+			break;
+
+	case"Abril":
+	case"Junio":
+	case"Septiembre":
+	case"Noviembre":
+
+			alert("tiene 30 dias");
+
+			break;
 
 //alert (mesDelAño);
 	
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,11 +1,22 @@
 function Mostrar()
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
 	
-	
+var laHora = document.getElementById('hora').value;
+	laHora= parseInt(laHora); // si no lo pongo al parseInt()
+								// en los "cases" tengo q poner comillas.
+
+switch (laHora)
+
+	{ 	case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+
+		alert("es de mañana");
+
+		break;}
 
 
 
